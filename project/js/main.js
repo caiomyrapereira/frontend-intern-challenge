0877954fefe0f682b277d8c7bfda5d7c2a95d6ff
caio myra pereira <caiomyrapereira@hotmail.com>
2019-11-16 21:06:46 -0300

    encurtar link

--- project/js/main.js
+++ project/js/main.js
@@ -6,53 +6,102 @@
 
     const app = (function() {
         return {
+
             initialize: function() {
                 this.initEvent();
             },
+
             initEvent: function() {
+                this.get();
+            },
+
+            get: function get() {
                 const ajax = new XMLHttpRequest();
                 const arq = '/Assets/urls.json';
                 ajax.open('GET', arq);
                 ajax.send();
-                ajax.addEventListener('readystatechange', function() {
-
-                    if (app.isRequestOk(this)) {
-                        const data = JSON.parse(ajax.responseText);
-                        console.log('requesição ok! \n' + data)
-                        app.top5(data)
-                        app.totalHits(data)
-                    }
-
-                }, false)
+                ajax.addEventListener('readystatechange', this.response);
             },
+
+            response: function response() {
+                if (app.isRequestOk(this)) {
+                    const data = JSON.parse(this.responseText);
+                    app.top5(data);
+                    app.totalHits(data);
+                    app.encurtar(data);
+                }
+            },
+
             isRequestOk: function isRequestOk(ajax) {
                 return ajax.readyState === 4 && ajax.status === 200;
             },
+
             top5: function top5(data) {
-                const links = doc.querySelectorAll('[data-js="links"]');
-                const hits = doc.querySelectorAll('[data-js="hits"]');
+                const $links = doc.querySelectorAll('[data-js="links"]');
+                const $hits = doc.querySelectorAll('[data-js="hits"]');
                 const top5 = data.sort(function(a, b) {
                     return b.hits - a.hits;
                 }).slice(0, 5);
 
                 top5.forEach(function(elem, index) {
-                    links[index].innerHTML = elem.shortUrl;
-                    hits[index].innerHTML = app.formatarHits(elem.hits);
+                    $links[index].innerHTML = elem.shortUrl;
+                    $hits[index].innerHTML = app.formatarHits(elem.hits);
                 })
             },
-            totalHits: function totalHits(data) {
-                const total = doc.querySelector('[data-js="totalHits"]');
 
-                total.innerHTML = app.formatarHits(data.reduce(function(atual, acom) {
+            totalHits: function totalHits(data) {
+                const $total = doc.querySelector('[data-js="totalHits"]');
+                $total.innerHTML = app.formatarHits(data.reduce(function(atual, acom) {
                     return atual + acom.hits;
                 }, 0));
             },
+
+            encurtar: function encurtar(data) {
+                const $encurtar = doc.querySelector('[data-js="encurtar"]');
+                const $close = doc.querySelector('[data-js="close"]');
+                const $inputUrl = doc.querySelector('[data-js="inputUrl"]');
+                $encurtar.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    data.forEach((elem) => app.buttonClose(elem, $encurtar, $close, $inputUrl));
+                    app.copyUrl($encurtar, $close, $inputUrl);
+                })
+            },
+
+            buttonClose: function buttonClose(elem, $encurtar, $close, $inputUrl) {
+                if (elem.url === $inputUrl.value) {
+                    $inputUrl.value = elem.shortUrl;
+                    $close.setAttribute('style', 'visibility:visible;');
+                    $inputUrl.setAttribute('style', 'color:white;');
+                    $inputUrl.setAttribute('disabled', 'true');
+
+                    $close.addEventListener('click', function(e) {
+                        e.preventDefault();
+                        $close.setAttribute('style', 'visibility:collapse;');
+                        $inputUrl.value = '';
+                        $encurtar.innerHTML = 'Encutar';
+                        $encurtar.setAttribute('data-js', 'Encutar');
+                        $inputUrl.removeAttribute('disabled')
+                        $inputUrl.setAttribute('style', 'color: #fe6f14;')
+                    })
+                }
+            },
+
+            copyUrl: function copyUrl($encurtar, $close, $inputUrl) {
+                if ($close.style.visibility === 'visible') {
+                    $encurtar.innerHTML = 'Copiar';
+                    $encurtar.setAttribute('data-clipboard-text', $inputUrl.value);
+                    $encurtar.setAttribute('data-js', 'copy');
+                    const clipboard = new ClipboardJS('[data-js="copy"]');
+                    clipboard.on('success', function(e) {
+                        console.log("Texto copiado")
+                    });
+                }
+            },
+
             formatarHits: function formatarHits(hits) {
                 return hits.toLocaleString('pt-BR');
             }
-
         };
-
     })();
 
     app.initialize();
